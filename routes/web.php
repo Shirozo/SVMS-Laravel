69126cadc5021e5541a5a184c60fe124813ca76a
Shirozo <caranzoneilbryan@gmail.com>
2024-06-20 15:31:16 +0800

    Added route for Voters

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CollegeController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\VoteController;
+use App\Http\Controllers\VoterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, "index"])->name("index");
@@ -10,5 +11,7 @@ Route::get('/', [IndexController::class, "index"])->name("index");
 Route::get('/vote', [VoteController::class, "votes"])->name("votes");
 
 Route::get("/college", [CollegeController::class, "college"])->name("college");
+
+Route::get("/voters", [VoterController::class, "voters"])->name("voters");
 
 require __DIR__ . '/auth.php';
