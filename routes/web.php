9246a26749ab7cdf82dd693d8c4e53df1250f165
Shirozo <caranzoneilbryan@gmail.com>
2024-06-20 23:47:24 +0800

    added auth middleware'

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,21 @@ use App\Http\Controllers\VoteController;
 use App\Http\Controllers\VoterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, "index"])->name("index");
+Route::get('/', [IndexController::class, "index"])->name("index")
+    ->middleware('auth');
 
-Route::get('/vote', [VoteController::class, "votes"])->name("votes");
+Route::get('/vote', [VoteController::class, "votes"])->name("votes")
+    ->middleware('auth');
 
-Route::get("/college", [CollegeController::class, "college"])->name("college");
+Route::get("/college", [CollegeController::class, "show"])->name("college.index")
+    ->middleware('auth');
+// ->middleware('can:Gatename'); For Auth
 
-Route::get("/voters", [VoterController::class, "voters"])->name("voters");
+Route::post("/college", [CollegeController::class, "store"])
+    ->middleware('auth')
+    ->name("college.create");
+
+Route::get("/voters", [VoterController::class, "voters"])->name("voters")
+    ->middleware('auth');
 
 require __DIR__ . '/auth.php';
