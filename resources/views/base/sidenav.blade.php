0b7c78b87287b9f85c43f9abd653b5ee25170941
Shirozo <caranzoneilbryan@gmail.com>
2024-06-21 00:40:07 +0800

    URL rename

--- resources/views/base/sidenav.blade.php
+++ resources/views/base/sidenav.blade.php
@@ -20,7 +20,7 @@
 
             <li class="header">MANAGE</li>
             <li>
-                <a href="{{ route('college') }}"><i class="fa fa-university"></i><span>Colleges</span></a>
+                <a href="{{ route('college.index') }}"><i class="fa fa-university"></i><span>Colleges</span></a>
             </li>
             <li>
                 <a href="{{ route('voters') }}"><i class="fa fa-users"></i><span>Voters</span></a>
