bc026ce5355e954db82794b4df9b2fec454bfa8b
Shirozo <caranzoneilbryan@gmail.com>
2024-06-21 20:37:43 +0800

    Uses action naming convention:

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CollegeController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\VoteController;
 use App\Http\Controllers\VoterController;
+use App\Models\College;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, "index"])->name("index")
@@ -15,10 +16,13 @@ Route::get('/vote', [VoteController::class, "votes"])->name("votes")
 Route::get("/college", [CollegeController::class, "show"])->name("college.index")
     ->middleware('auth');
 // ->middleware('can:Gatename'); For Auth
-
 Route::post("/college", [CollegeController::class, "store"])
     ->middleware('auth')
-    ->name("college.create");
+    ->name("college.store");
+
+Route::delete("college/delete/{id}", [CollegeController::class, "destroy"])
+    ->middleware("auth")
+    ->name("college.destroy");
 
 Route::get("/voters", [VoterController::class, "voters"])->name("voters")
     ->middleware('auth');
