e050f6d0fe12c742a179e0ee4efa097f1e0a1ecf
Shirozo <caranzoneilbryan@gmail.com>
2024-06-22 18:41:22 +0800

    Added auth toastr

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Flasher\Prime\Notification\Type;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -14,6 +15,7 @@ class AuthenticatedSessionController extends Controller
     public function showForm() {
 
         if (Auth::user()) {
+            toastr("Welcome back!", Type::SUCCESS);
             return redirect()->intended(route("index"));
         }
 
@@ -29,6 +31,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        toastr("Welcome!", Type::SUCCESS);
         return redirect()->intended(route("index"));
     }
 
