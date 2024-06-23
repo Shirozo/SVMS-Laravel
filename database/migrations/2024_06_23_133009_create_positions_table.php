91c404ad7c4ad397b1b1879dea378b2d13fb7081
Shirozo <caranzoneilbryan@gmail.com>
2024-06-23 22:11:35 +0800

    Position database schema

---
+++ database/migrations/2024_06_23_133009_create_positions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->char("name", 50)->unique();
+            $table->unsignedInteger("max_vote");
+            $table->unsignedSmallInteger("priority");
+            $table->enum("exclusive", [0, 1]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('positions');
+    }
+};
