bb583ea39e6f87f99a3d77ecd8a664ef519aa371
Shirozo <caranzoneilbryan@gmail.com>
2024-06-25 23:03:30 +0800

    Added fillable for election model

---
+++ app/Models/Election.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Election extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "scope",
+        "college_limit",
+        "course_limit",
+        "year_level_limit"
+    ];
+}
