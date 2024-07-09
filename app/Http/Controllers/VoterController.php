262311ec08a7eb6eb248d1d82e68ef1f2432efe0
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-09 22:01:18 +0800

    Implemented all the necessary function for uploading and inserting the data

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\VoterUpload;
 use App\Models\Course;
 use App\Models\User;
-use Error;
 use Flasher\Prime\Notification\Type;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
@@ -153,6 +156,22 @@ class VoterController extends Controller
         return redirect()->route('voters.index');
     }
 
+    public function destroy(Request $request)
+    {
+        $user_id = $request->user_del;
+        $user_data = User::find($user_id);
+
+        if ($user_data != null) {
+
+            $user_data->delete();
+            toastr("User has been deleted!", Type::SUCCESS);
+        } else {
+            toastr("User is not registered!", Type::ERROR);
+        }
+
+        return redirect()->route("voters.index");
+    }
+
     public function api(Request $request)
     {
         $id = $request->voter_id;
@@ -169,19 +188,123 @@ class VoterController extends Controller
         ], 404);
     }
 
-    public function destroy(Request $request)
+    public function upload(Request $request)
     {
-        $user_id = $request->user_del;
-        $user_data = User::find($user_id);
+        if ($request->has('v_file')) {
 
-        if ($user_data != null) {
+            try {
+                $filenameWithExt = $request->file('v_file')->getClientOriginalName();
 
-            $user_data->delete();
-            toastr("User has been deleted!", Type::SUCCESS);
+                $file_name = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+
+                $currentDate = date('Y-m-d-H-i-s');
+
+                $username = Auth::user()->username;
+
+                $r_path = $currentDate . "-" . $file_name . "-" . $username;
+
+                $folderPath = 'uploads/' . $r_path;
+
+                $folderFullPath = resource_path($folderPath);
+
+
+                if (!File::exists($folderFullPath)) {
+                    // Create the folder
+                    File::makeDirectory($folderFullPath, 0755, true);
+                } else {
+                    return response()->json([
+                        "message" => "Please try again after 1 minute.",
+                    ], 500);
+                }
+
+                $data = file($request->v_file);
+                $header = $data[0];
+                unset($data[0]);
+
+                $chunks = array_chunk($data, 50);
+
+                $count = 1;
+
+                foreach ($chunks as $key => $chunk) {
+                    $name = "{$count}.csv";
+                    array_unshift($chunk, $header);
+                    $path = $folderFullPath . '/' . $name;
+
+                    file_put_contents($path, implode("", $chunk));
+
+                    $count++;
+                }
+
+                return response()->json([
+                    "message" => "Registering Voters Now...",
+                    "folder" => $r_path
+                ], 200);
+            } catch (\Throwable $th) {
+                if (File::exists($folderFullPath)) {
+                    // Create the folder
+                    File::deleteDirectory($folderFullPath);
+                }
+                return response()->json([
+                    "message" => "Internal Server Error!",
+                ], 500);
+            }
         } else {
-            toastr("User is not registered!", Type::ERROR);
+            return response()->json([
+                "message" => "Action not allowed",
+            ], 404);
         }
-
-        return redirect()->route("voters.index");
     }
+
+
+    public function registerVoters(Request $request)
+    {
+        try {
+            $path = $request->v;
+
+            $baseDirectory = resource_path('uploads');
+
+            $csvPattern = $baseDirectory . "/" . $path . "/*";
+
+            $files = glob($csvPattern);
+
+            if ($files) {
+                $header = [];
+                $batches = Bus::batch([])->dispatch();
+                $c = 0;
+
+                foreach ($files as $key => $file) {
+                    $data = array_map('str_getcsv', file($file));
+
+                    if ($key === $c) {
+                        $header = $data[0];
+                        unset($data[0]);
+                    }
+                    $c++;
+
+                    $batches->add(new VoterUpload($data, $header));
+                }
+
+                
+                return response()->json([
+                    "id" => $batches->id,
+                ], 200);
+            }
+            else {
+                return response()->json([
+                    "message" => "Invalid Argument!"
+                ], 403);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th
+            ], 403);
+        }
+    }
+
+    public function progress(Request $request)
+    {
+        $batch_id = $request->batch_id;
+        return Bus::findBatch($batch_id);
+    }
+
 }
