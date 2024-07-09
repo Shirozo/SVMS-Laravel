d15a86ceb517855bd75c7c427e30fb47a2aa3b87
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-09 22:02:54 +0800

    queue work for voter registration

---
+++ app/Jobs/VoterUpload.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class VoterUpload implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+    public $header;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data, $header)
+    {
+        $this->data = $data;
+        $this->header = $header;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->data as $d) {
+            $voter = array_combine($this->header, $d);
+            User::create($voter);
+        }
+    }
+}
