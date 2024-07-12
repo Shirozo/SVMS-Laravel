2a231e8fa592a4478e11b7ed8c06055c568a764b
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-12 21:28:23 +0800

    Added started as fillable

--- app/Models/Election.php
+++ app/Models/Election.php
@@ -14,6 +14,7 @@ class Election extends Model
         "scope",
         "college_limit",
         "course_limit",
-        "year_level_limit"
+        "year_level_limit",
+        "started"
     ];
 }
