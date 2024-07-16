ec187a0a85be7601a626154b93208a136abde869
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-16 22:24:32 +0800

    ELection Histpory model

---
+++ app/Models/ElectionData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Sofa\Eloquence\Eloquence;
+
+class ElectionData extends Model
+{
+    use HasFactory, Eloquence;
+
+    protected $searchableColumns = ['voter_name'];
+
+    protected $fillable = [
+        "voter_name",
+        "voter_id",
+        "election_id",
+    ];
+}
