e776a7ca5a9f2266d13dc74e55a7a44059de974a
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-16 22:24:15 +0800

    Candidate model

---
+++ app/Models/Candidate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "fullname",
+        "photo",
+        "bio",
+        "user_id",
+        "position_id",
+        "election_id"
+    ];
+}
