4fb04be9d0c085d8afc3e0ea3425dc694c579efc
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-07-16 22:22:37 +0800

    schema for candidates

---
+++ database/migrations/2024_07_12_154153_create_candidates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->char("fullname", 150);
+            $table->text("photo")->nullable(true);
+            $table->text("bio");
+            
+            //Foreign Key
+            $table->foreignId("user_id")->constrained(table: "users", column: "id")->onUpdate("cascade")->onDelete('cascade');
+            $table->foreignId("position_id")->constrained(table: "positions", column: "id")->onUpdate("cascade")->onDelete('cascade');
+            $table->foreignId("election_id")->constrained(table: "elections", column: "id")->onUpdate("cascade")->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
