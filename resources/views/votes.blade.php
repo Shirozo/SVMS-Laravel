0499f95a0aed2cadf5371143d60d4f23e0166529
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-08-12 12:20:41 +0800

    render data

--- resources/views/votes.blade.php
+++ resources/views/votes.blade.php
@@ -1,34 +1,134 @@
-@extends('base.base')
+@extends('base.electionbase')
 
-@section("title")
-Votes
+@section('title')
+    Votes
+@endsection
+
+@section('action')
+    Result
 @endsection
 
 @section('main')
     <section class="content">
         <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <a href="#reset" data-toggle="modal" class="btn btn-danger btn-sm btn-flat">
-                            <i class="fa fa-refresh"></i> Reset
-                        </a>
+            <div class="col-lg-3 col-xs-6">
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                        <h3>Voters</h3>
+                        <p>No. of Count</p>
                     </div>
-                    <div class="box-body">
-                        <table id="example1" class="table table-bordered table-hover table-striped">
-                            <thead style="background-color: #222D32; color:white;">
-                                <th>Voter's Name</th>
-                                <th>Candidate Voted For</th>
-                                <th>Position</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Sample</td>
-                                    <td>Data</td>
-                                    <td>Votes</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="icon">
+                        <i class="fa fa-tasks"></i>
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-xs-6">
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>Voted</h3>
+                        <p>No. of Count</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-check"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-xs-6">
+                <div class="small-box bg-maroon">
+                    <div class="inner">
+                        <h3>Canidates</h3>
+                        <p>No. of Count</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <div class="result-container">
+                <div class="col-xs-12 result-left">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3>Live Result</h3>
+                        </div>
+                        <div class="result-chart">
+                            @foreach ($new_data as $key => $nd)
+                                <div class="chart-container" style="position: relative; height:40vh; width:80vw">
+                                    <canvas id="{{ $key }}" height="250px"></canvas>
+                                </div>
+                                <script>
+                                    document.addEventListener("DOMContentLoaded", async function() {
+                                        const ctx = document.getElementById('{{ $key }}');
+                                        const labels = @json(array_map(fn($item) => $item['c_name'], $nd));
+                                        const data = @json(array_map(fn($item) => $item['c_votes'], $nd));
+
+                                        new Chart(ctx, {
+                                            type: 'bar',
+                                            data: {
+                                                labels: labels,
+                                                datasets: [{
+                                                    axis: 'y',
+                                                    label: '{{ $key }}',
+                                                    data: data,
+                                                    fill: false,
+                                                    backgroundColor: [
+                                                        'rgba(255, 99, 132, 0.2)',
+                                                        'rgba(255, 159, 64, 0.2)',
+                                                        'rgba(255, 205, 86, 0.2)',
+                                                        'rgba(75, 192, 192, 0.2)',
+                                                        'rgba(54, 162, 235, 0.2)',
+                                                        'rgba(153, 102, 255, 0.2)',
+                                                        'rgba(201, 203, 207, 0.2)'
+                                                    ],
+                                                    borderColor: [
+                                                        'rgb(255, 99, 132)',
+                                                        'rgb(255, 159, 64)',
+                                                        'rgb(255, 205, 86)',
+                                                        'rgb(75, 192, 192)',
+                                                        'rgb(54, 162, 235)',
+                                                        'rgb(153, 102, 255)',
+                                                        'rgb(201, 203, 207)'
+                                                    ],
+                                                    borderWidth: 1
+                                                }]
+                                            }
+                                        });
+                                    })
+                                </script>
+                            @endforeach
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-xs-12 result-right">
+                    <div class="box" style="padding: 1% 1%">
+                        <div class="box-header with-border">
+                            <h3>Leaderboard</h3>
+                        </div>
+                        <div class="result-container">
+                            <table class="table">
+                                <thead>
+                                    <th>Position</th>
+                                    <th>Candidate</th>
+                                    <th>Vote</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($highest as $h => $data)
+                                        <tr>
+                                            <td>{{ $h }}</td>
+                                            @if ($highest[$h]['votes'] > 0)
+                                                <td>{{ $highest[$h]['name'] }}</td>
+                                                <td>{{ $highest[$h]['votes'] }}</td>
+                                            @else
+                                                <td>None</td>
+                                                <td>None</td>
+                                            @endif
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -63,4 +163,43 @@ Votes
             </div>
         </div>
     </div>
+@endsection
+
+@section('custom_script')
+    <script>
+        document.addEventListener("DOMContentLoaded", async function() {
+            const ctx = document.getElementById('sample-data');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['January', 'February'],
+                    datasets: [{
+                        axis: 'y',
+                        label: 'My First Dataset',
+                        data: [65, 59],
+                        fill: false,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(201, 203, 207, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)',
+                            'rgb(153, 102, 255)',
+                            'rgb(201, 203, 207)'
+                        ],
+                        borderWidth: 1
+                    }]
+                }
+            });
+        })
+    </script>
 @endsection
