587947c377a1334676d286e7ea233ed0c02e6d84
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-08-18 22:01:06 +0800

    Implemented gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,16 +25,28 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        // Gate::define("college", function($user) {
-        //     Logic
-        // });
 
-        // Two5 auth
-        // Gate::define("name", function($param1, $para) {
-        //     Logic || Logic
-        // });
+        Gate::define("level1", function (User $user): bool {
+            if ($user->user_type == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        Gate::define("level2", function (User $user): bool {
+            if ($user->user_type == 2 || $user->user_type == 1) {
+                return true;
+            } else {
+                return false ;
+            }
+        });
+
+        Gate::define("level3", function (User $user): bool {
+            return true;
+        });
     }
 }
